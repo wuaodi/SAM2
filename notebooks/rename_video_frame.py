9a1594d36ba828cb2bb01ed094f3ba123416dea0
wuaodi <2582469681@qq.com>
2024-09-05 11:05:47 +0800

    将视频帧重命名库换乘opencv

--- notebooks/rename_video_frame.py
+++ notebooks/rename_video_frame.py
@@ -1,8 +1,9 @@
 import os
-from PIL import Image
+import cv2
 
 # 定义视频目录
-video_dir = "./videos/spacecraft"
+video_dir = "./videos/davinci_raofei"
+target_dir =    "./videos/davinci_raofei_3jpg"
 
 # 获取所有 JPEG 文件名并进行排序
 frame_names = [
@@ -14,13 +15,16 @@ frame_names.sort()
 # 重命名并缩放图像
 for idx, old_name in enumerate(frame_names, start=1):
     # 生成新的文件名
-    new_name = f"{idx:06d}.jpg"  # 格式化为 000001, 000002, ...
+    new_name = f"{idx:05d}.jpg"  # 格式化为 00001, 00002, ...
     old_path = os.path.join(video_dir, old_name)
-    new_path = os.path.join(video_dir, new_name)
+    new_path = os.path.join(target_dir, new_name)
 
     # 打开图像并缩放
-    with Image.open(old_path) as img:
-        img = img.resize((1024, 1024))  # 缩放到 1024x1024
-        img.save(new_path)  # 保存为新的文件名
+    img = cv2.imread(old_path)
+    img = cv2.resize(img, (1224, 1024))
+    # 打印通道数
+    print(img.shape)
+    cv2.imwrite(new_path, img)
+
 
 print("文件重命名和缩放完成。")
